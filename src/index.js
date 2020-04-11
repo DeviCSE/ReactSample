b6b618e6e0ccceb72429ae034c491320e1c2546a
Devi Das <devidas.cse07@gmail.com>
2020-04-11 15:15:59 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Counters from './components/counters';
+import 'bootstrap/dist/css/bootstrap.css'
+
+ReactDom.render(<Counters />, document.getElementById('root'));
